67679a8b0d17de5417c0c15bc293c69c1f8b837c
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-08-07 10:55:53 +1200

    server update

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World');
+  console.log(`${req.method} request for ${req.url}`);
+  if(req.url === '/') {
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.end(`
+    //   <html>
+    //     <head>
+    //       <title>This is our home page</title
+    //     </head>
+    //     <body>
+    //       <h1>This is our home page</h1>
+    //       <p>${req.method} request for ${req.url}</p>
+    //     </body>
+    //   </html>
+    //   `);
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('404 file not found');
+  }
   console.log('Your server is running on port 3000');
 }).listen(3000);
 
