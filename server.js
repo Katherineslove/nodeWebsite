2dc36fae04660b53131dfeebd4c763cd26e50709
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-08-07 11:20:34 +1200

    server udpates

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 const http = require('http');
 const fs = require('fs');
 
+
 http.createServer(function(req, res){
   console.log(`${req.method} request for ${req.url}`);
   if(req.url === '/') {
-    // res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.end(`
-    //   <html>
-    //     <head>
-    //       <title>This is our home page</title
-    //     </head>
-    //     <body>
-    //       <h1>This is our home page</h1>
-    //       <p>${req.method} request for ${req.url}</p>
-    //     </body>
-    //   </html>
-    //   `);
+    fs.readFile('./public/index.html', 'UTF-8', function(err, data){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    })
+  } else if (req.url === '/css/style.css') {
+    fs.readFile('./public/css/style.css', 'UTF-8', function(err, data){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.end(data);
+    })
+  } else if (req.url === '/js/script.js') {
+    fs.readFile('./public/js/script.js', 'UTF-8', function(err, data){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/js'});
+      res.end(data);
+    })
   } else {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('404 file not found');
   }
+
   console.log('Your server is running on port 3000');
+
 }).listen(3000);
 
 console.log('The server is running on port 3000');
